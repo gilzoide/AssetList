bbb1128954159559579a7744ef3e23286bd1503f
gilzoide <gilzoide@gmail.com>
2022-06-04 11:38:17 -0300

    Use partial class to separate Editor code

--- Runtime/AssetList.cs
+++ Runtime/AssetList.cs
@@ -1,69 +1,93 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+#if UNITY_EDITOR
+using System.Linq;
 using UnityEditor;
+#endif
 
 namespace Gilzoide.AssetList
 {
     [CreateAssetMenu(menuName = "AssetList", fileName = "AssetList")]
-    public class AssetList : ScriptableObject
+    public partial class AssetList : ScriptableObject
     {
+        /// <summary>
+        /// An <cref>AssetDatabase</cref> search filter.
+        /// If empty, the <cref>Assets</cref> list will be empty.
+        /// </summary>
+        [Tooltip("An AssetDatabase search filter. If empty, the Assets list will be empty")]
         public string SearchFilter;
+
+        /// <summary>
+        /// List of assets found by <cref>AssetDatabase.FindAssets</cref> using the
+        /// <cref>SearchFilter</cref>.
+        /// </summary>
         public List<Object> Assets;
+    }
 
 #if UNITY_EDITOR
-        [ContextMenu("Update List")]
-        void UpdateList()
+    public partial class AssetList
+    {
+        /// <summary>Update list when the Unity editor first loads this asset</summary>
+        void Awake()
+        {
+            UpdateList();
+        }
+
+        /// <summary>Update list when this asset gets enabled in Play mode</summary>
+        void OnEnable()
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                UpdateList();
+            }
+        }
+
+        /// <summary>
+        /// Update the <cref>Assets</cref> list, based on the configured <cref>SearchFilter</cref>.
+        /// </summary>
+        [ContextMenu("Update Assets List")]
+        internal void UpdateList()
         {
             if (string.IsNullOrEmpty(SearchFilter))
             {
+                Assets.Clear();
                 return;
             }
 
             Assets = AssetDatabase.FindAssets(SearchFilter)
-                .OrderBy(Identity)
+                .OrderBy(x => x)
                 .Select(AssetDatabase.GUIDToAssetPath)
                 .Select(AssetDatabase.LoadAssetAtPath<Object>)
                 .ToList();
             EditorUtility.SetDirty(this);
         }
+    }
 
-        void OnEnable()
+    [CustomEditor(typeof(AssetList)), CanEditMultipleObjects]
+    class AssetListEditor : Editor 
+    {
+        public override void OnInspectorGUI()
         {
-            UpdateList();
-        }
-
-        T Identity<T>(T x)
-        {
-            return x;
-        }
-
-        [CustomEditor(typeof(AssetList)), CanEditMultipleObjects]
-        public class AssetListEditor : Editor 
-        {
-            public override void OnInspectorGUI()
+            serializedObject.Update();
+            SerializedProperty property = serializedObject.GetIterator();
+            property.NextVisible(true);
+            do
             {
-                serializedObject.Update();
-                SerializedProperty property = serializedObject.GetIterator();
-                property.NextVisible(true);
-                do
+                using (new EditorGUI.DisabledScope(property.name == "m_Script"))
                 {
-                    using (new EditorGUI.DisabledScope(property.name == "m_Script"))
+                    EditorGUILayout.PropertyField(property);
+                }
+                if (property.name == nameof(AssetList.SearchFilter) && GUILayout.Button("Update Assets List"))
+                {
+                    foreach (Object obj in targets)
                     {
-                        EditorGUILayout.PropertyField(property);
-                    }
-                    if (property.name == nameof(SearchFilter) && GUILayout.Button("Update List"))
-                    {
-                        foreach (Object obj in targets)
-                        {
-                            (obj as AssetList).UpdateList();
-                        }
+                        (obj as AssetList).UpdateList();
                     }
                 }
-                while (property.NextVisible(false));
-                serializedObject.ApplyModifiedProperties();
             }
+            while (property.NextVisible(false));
+            serializedObject.ApplyModifiedProperties();
         }
-#endif
     }
+#endif
 }
